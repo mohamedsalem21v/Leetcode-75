e22064b3cab70e0f03221ad0d056880ac73234f0
Mohammed Salem <mohamedsalem462006@gmail.com>
2025-10-02 19:12:17 +0300

    [724] [Time Beats: 100.00%] [Memory Beats: 95.74%] - LeetPush

---
+++ Prefix Sum/Find-Pivot-Index.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int left = 0;
+        int sum = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            sum += nums[i];
+        }
+        for(int i = 0; i < nums.size(); i++)
+        {
+            int right = sum - left - nums[i];
+            if(left == right)
+                return i;
+            left += nums[i];
+        }
+        return -1;
+    }
+};
