0736402970ea253355fd42e0f3bf29b214b53f7a
Mohammed Salem <mohamedsalem462006@gmail.com>
2025-10-02 19:05:12 +0300

    [1431] [Time Beats: 100.00%] [Memory Beats: 0.95%] - LeetPush

---
+++ Array / String/Kids-With-the-Greatest-Number-of-Candies.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool> v(candies.size(),false);
+        int mx = *max_element(candies.begin(), candies.end());
+        for(int i = 0; i < candies.size(); i++)
+        {
+            if(candies[i]+extraCandies >= mx)
+                v[i] = true;
+        }
+        return v;
+    }
+};
